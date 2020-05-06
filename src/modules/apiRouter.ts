2444f6c448f16fe7fbfa87dd26ef491f4438710f
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-06 18:39:55 +0300

    feat: added a router for auth handling

--- src/modules/apiRouter.ts
+++ src/modules/apiRouter.ts
@@ -1,9 +1,11 @@
 import Router from "./Router"
 
 import userRouter from "./user/router"
+import authRouter from "./auth/router"
 
 const apiRouter = new Router({ prefix: "/api" })
 
 apiRouter.use(userRouter)
+apiRouter.use(authRouter)
 
 export default apiRouter.routes()
