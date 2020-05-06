bb6bbca1ec4ac35f055a032cbaefa7f980f92e80
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-06 10:20:13 +0300

    feat: added a schema validator

---
+++ src/modules/schema/helpers/validateSchema.ts
@@ -0,0 +1,24 @@
+import { Middleware } from "koa"
+
+import { ObjectSchema } from "@hapi/joi"
+
+import { HttpError } from "../../error/classes/HttpError"
+
+type ValidationField = "body" | "params" | "query"
+
+export const validateSchema = (
+    schema: ObjectSchema,
+    field: ValidationField
+): Middleware => (ctx, next) => {
+    const toValidate: Record<ValidationField, any> = {
+        body: ctx.request.body,
+        params: ctx.params,
+        query: ctx.query
+    }
+
+    const result = schema.validate(toValidate[field])
+
+    if (result.error) throw new HttpError(400, "Invalid Syntax")
+
+    return next()
+}
