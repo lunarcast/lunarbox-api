b2067407da79a596d1f62b3e84f63a503c24d463
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-06 18:50:16 +0300

    fix: added check for taken username or email

--- src/modules/user/actions/createUser.ts
+++ src/modules/user/actions/createUser.ts
@@ -2,5 +2,15 @@ import { db } from "../../../../db/knex"
 
 import { User, EditableUser } from "../types/User"
 
-export const createUser = async (user: EditableUser) =>
-    (await db<User>("users").insert(user, "*"))[0]
+export const createUser = async (user: EditableUser) => {
+    const isTaken = Boolean(
+        await db<User>("users")
+            .select("id")
+            .where({ username: user.username })
+            .orWhere({ email: user.email })
+            .first()
+    )
+    if (isTaken) return null
+
+    return (await db<User>("users").insert(user, "*"))[0]
+}
