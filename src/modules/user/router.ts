6df966ae849ecc6767841f0962ffdc4f4abba9da
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-06 18:42:33 +0300

    feat: added a method for fetching the current user and a method for deleting the current user

--- src/modules/user/router.ts
+++ src/modules/user/router.ts
@@ -13,23 +13,58 @@ import { validateSchema } from "../schema/middleware/validateSchema"
 import { registerBody } from "./schema/registerBody"
 import { RegisterBody } from "./types/RegisterBody"
 
+import { requireAuthenticated } from "../auth/middleware/requireAuthenticated"
+import { requireUnauthenticated } from "../auth/middleware/requireUnauthenticated"
+
 const router = new Router({ prefix: "/users" })
 
-router.post("/", validateSchema(registerBody, "body"), async (ctx, next) => {
-    const { email, username, password } = ctx.request.body as RegisterBody
+router.get("/", requireAuthenticated(), async (ctx, next) => {
+    const userId = ctx.session!.user
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = await createUser({
-        email,
-        username,
-        password: hashedPassword
-    })
+    const user = await findUser("id", userId)
     if (!user) {
-        throw new HttpError(400, "That Username seems to be already taken")
+        throw new HttpError(404, "There seems to be no user with that id.")
     }
 
-    ctx.status = 201
-    ctx.body = { status: 201, message: "Successfully created" }
+    const { email, username } = user
+
+    ctx.status = 200
+    ctx.body = { email, username }
+
+    await next()
+})
+
+router.post(
+    "/",
+    requireUnauthenticated(),
+    validateSchema(registerBody, "body"),
+    async (ctx, next) => {
+        const { email, username, password } = ctx.request.body as RegisterBody
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await createUser({
+            email,
+            username,
+            password: hashedPassword
+        })
+        if (!user) {
+            throw new HttpError(400, "That Username seems to be already taken")
+        }
+
+        ctx.status = 201
+        ctx.body = { status: 201, message: "Successfully created" }
+
+        await next()
+    }
+)
+
+router.delete("/", requireAuthenticated(), async (ctx, next) => {
+    const userId = ctx.session!.user as number
+
+    await deleteUser(userId)
+
+    ctx.status = 200
+    ctx.body = { status: 200, message: "Successfully deleted user account" }
 
     await next()
 })
