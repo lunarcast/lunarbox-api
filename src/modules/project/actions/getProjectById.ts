3a52c6ade966c1bdd6d840be6e7d9063120f637e
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-06 19:06:16 +0300

    fix: renamed exported function for fetching a project by id

--- src/modules/project/actions/getProjectById.ts
+++ src/modules/project/actions/getProjectById.ts
@@ -2,5 +2,5 @@ import { db } from "../../../../db/knex"
 
 import { Project } from "../types/Project"
 
-export const getProjects = (id: Project["id"]) =>
+export const getProjectById = (id: Project["id"]) =>
     db<Project>("projects").select().first().where({ id })
