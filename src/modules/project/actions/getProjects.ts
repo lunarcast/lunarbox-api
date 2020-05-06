400181037a032463aa5a1f9f3060d164bcf078e0
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-06 19:25:19 +0300

    feat: made user projects fetching return additional properties

--- src/modules/project/actions/getProjects.ts
+++ src/modules/project/actions/getProjects.ts
@@ -4,13 +4,27 @@ import { User } from "../../user/types/User"
 import { Project } from "../types/Project"
 
 export const getProjects = async (id: User["id"]) => {
-    const exampleProjects = await db<Project>("projects")
-        .select()
-        .where({ example: true })
+    const exampleProjects = (
+        await db<Project>("projects")
+            .select(["id", "name", "project"])
+            .where({ example: true })
+    ).map(e => {
+        const { project: proj } = e
+        const { nodeCount, functionCount } = proj
 
-    const userProjects = await db<Project>("projects")
-        .select()
-        .where({ owner: id })
+        return { ...e, nodeCount, functionCount }
+    })
+
+    const userProjects = (
+        await db<Project>("projects")
+            .select(["id", "name", "project"])
+            .where({ owner: id })
+    ).map(e => {
+        const { project: proj } = e
+        const { nodeCount, functionCount } = proj
+
+        return { ...e, nodeCount, functionCount }
+    })
 
     return { exampleProjects, userProjects }
 }
