f14d06080ca145db9edf6aed4c9fd51c8f6c27c7
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-07 11:25:33 +0300

    feat: updated projectBody schema

--- src/modules/project/schema/projectBody.ts
+++ src/modules/project/schema/projectBody.ts
@@ -3,5 +3,6 @@ import Joi from "@hapi/joi"
 export const projectBody = Joi.object({
     name: Joi.string().min(2).max(48).required(),
     description: Joi.string().max(2048),
-    project: Joi.object()
+    project: Joi.object(),
+    metadata: Joi.object()
 })
