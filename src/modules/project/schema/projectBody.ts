36623131a06bd403953fbbdd6e7d9b98a72a573c
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-07 21:51:48 +0300

    fix: made project descriptions truly optional

--- src/modules/project/schema/projectBody.ts
+++ src/modules/project/schema/projectBody.ts
@@ -2,7 +2,7 @@ import Joi from "@hapi/joi"
 
 export const projectBody = Joi.object({
     name: Joi.string().min(2).max(48).required(),
-    description: Joi.string().max(2048),
+    description: Joi.string().max(2048).optional(),
     isEnabled: Joi.boolean(),
     project: Joi.object().required(),
     metadata: Joi.object().required()
