c201ab9954c82541af35b7b1aee18e85c396dc2a
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-07 09:39:45 +0300

    feat: descriptions are not required for projects

--- src/modules/project/schema/projectBody.ts
+++ src/modules/project/schema/projectBody.ts
@@ -2,6 +2,6 @@ import Joi from "@hapi/joi"
 
 export const projectBody = Joi.object({
     name: Joi.string().min(2).max(48).required(),
-    description: Joi.string().max(2048).required(),
+    description: Joi.string().max(2048),
     project: Joi.object()
 })
