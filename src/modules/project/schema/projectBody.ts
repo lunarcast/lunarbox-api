dcf6f64e38512aa21f24a47eb1eff22f13d141b3
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-07 21:52:19 +0300

    fix: renamed projectBody isEnabled prop to isExample

--- src/modules/project/schema/projectBody.ts
+++ src/modules/project/schema/projectBody.ts
@@ -3,7 +3,7 @@ import Joi from "@hapi/joi"
 export const projectBody = Joi.object({
     name: Joi.string().min(2).max(48).required(),
     description: Joi.string().max(2048).optional(),
-    isEnabled: Joi.boolean(),
+    isExample: Joi.boolean(),
     project: Joi.object().required(),
     metadata: Joi.object().required()
 })
