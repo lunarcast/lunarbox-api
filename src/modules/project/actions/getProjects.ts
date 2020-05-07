27dcee258d3630e7b414afa78e56c9f2da6dc761
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-07 22:13:44 +0300

    fix: fetching an user's projects no longer returns the actual project graph

--- src/modules/project/actions/getProjects.ts
+++ src/modules/project/actions/getProjects.ts
@@ -5,11 +5,11 @@ import { Project } from "../types/Project"
 
 export const getProjects = async (id: User["id"]) => {
     const exampleProjects = await db<Project>("projects")
-        .select(["id", "name", "project", "metadata"])
+        .select(["id", "name", "metadata"])
         .where({ example: true })
 
     const userProjects = await db<Project>("projects")
-        .select(["id", "name", "project", "metadata"])
+        .select(["id", "name", "metadata"])
         .where({ owner: id })
 
     return { exampleProjects, userProjects }
