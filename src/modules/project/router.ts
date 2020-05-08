3872aae92320d11b378303a0819f6fa5e0556c59
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-08 10:50:28 +0300

    feat: added a route for cloning projects

--- src/modules/project/router.ts
+++ src/modules/project/router.ts
@@ -50,6 +50,32 @@ router.get("/:id", requireAuthenticated(), async (ctx, next) => {
     await next()
 })
 
+router.get("/clone/:id", requireAuthenticated(), async (ctx, next) => {
+    const { id: projectId } = ctx.params.id as Pick<Project, "id">
+    const userId = ctx.session!.user
+
+    const project = await getProjectById(projectId)
+    if (!project) {
+        throw new HttpError(404, "Seems like there's no project with that id")
+    }
+
+    const createdProject = await createProject({
+        owner: userId,
+        example: false,
+        name: `${project.name} - clone`,
+        ...project
+    })
+
+    ctx.status = 201
+    ctx.body = {
+        status: 201,
+        message: "Successfully cloned",
+        project: { id: createdProject.id }
+    }
+
+    await next()
+})
+
 router.post(
     "/",
     requireAuthenticated(),
