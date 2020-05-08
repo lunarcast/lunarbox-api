e1bc0cbfc419d0ee279cfef3ce49d7014d9a7c2c
Matei Adriel <rafaeladriel11@gmail.com>
2020-05-08 17:57:49 +0300

    fix: fixed id not being read right when cloning projects

--- src/modules/project/router.ts
+++ src/modules/project/router.ts
@@ -51,7 +51,7 @@ router.get("/:id", requireAuthenticated(), async (ctx, next) => {
 })
 
 router.get("/clone/:id", requireAuthenticated(), async (ctx, next) => {
-    const { id: projectId } = ctx.params.id as Pick<Project, "id">
+    const { id: projectId } = ctx.params as Pick<Project, "id">
     const userId = ctx.session!.user
 
     const project = await getProjectById(projectId)
