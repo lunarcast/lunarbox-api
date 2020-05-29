977c10d88794f579140f496ea7ac999cd181f182
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-05-29 13:20:13 +0300

    feat: updated project body schema

--- src/modules/project/schema/projectBody.ts
+++ src/modules/project/schema/projectBody.ts
@@ -4,6 +4,7 @@ export const projectBody = Joi.object({
     name: Joi.string().min(2).max(48).required(),
     description: Joi.string().max(2048).optional(),
     isExample: Joi.boolean(),
+    visible: Joi.boolean(),
     project: Joi.object().required(),
     metadata: Joi.object().required()
 })
