3d9e8cd25d979cef9c5aaa020ef9a0cfd5a2f601
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-22 05:46:03 +0300

    fix: updated action for deleting tutorials

--- src/modules/tutorial/actions/deleteTutorial.ts
+++ src/modules/tutorial/actions/deleteTutorial.ts
@@ -1,9 +1,9 @@
 import { db } from "../../../../db/knex"
 
-import { Tutorial } from "../types/Tutorial"
+import { Tutorial, TutorialRaw } from "../types/Tutorial"
 
 export const deleteTutorial = async (id: Tutorial["id"]) => {
-    const result = await db<Tutorial>("tutorials").delete("*").where({ id })
+    const result = await db<TutorialRaw>("tutorials").delete("*").where({ id })
 
     return result
 }
