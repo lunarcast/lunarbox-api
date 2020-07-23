bfa83962140888e85a174086417286ec1f8a8a10
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-23 20:31:34 +0300

    feat: addded an owner column to tutorials

--- db/migrations/20200722053321_create-tutorials.ts
+++ db/migrations/20200722053321_create-tutorials.ts
@@ -3,12 +3,14 @@ import Knex from "knex"
 export const up = (knex: Knex) =>
     knex.schema.createTable("tutorials", table => {
         table.increments("id")
+        table.integer("owner")
         table.text("name")
         table.integer("base")
         table.json("requires")
         table.integer("solution")
         table.json("steps")
         table.json("hidden")
+        table.foreign("owner").references("id").inTable("users")
         table.foreign("base").references("id").inTable("projects")
         table.foreign("solution").references("id").inTable("projects")
     })
